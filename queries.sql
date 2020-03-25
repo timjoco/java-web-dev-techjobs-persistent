b6766a448598a5f175e5bef00e1fbf806405b6e0
Timjoco <timjoco13@gmail.com>
2020-03-25 14:22:47 -0500

    Updated queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,14 @@
 ## Part 1: Test it with SQL
+id, name, employer_id, jobs_id
 
 ## Part 2: Test it with SQL
+SELECT * FROM techjobs.employer
+WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
+DROP TABLE techjobs.job;
 
 ## Part 4: Test it with SQL
+SELECT * FROM techjobs.job, techjobs.skill
+WHERE techjobs.job.name = techjobs.skill.name & techjobs.skill.name IS NOT NULL
+ORDER BY techjobs.skill.name asc;
